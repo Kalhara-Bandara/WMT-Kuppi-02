d4b947fc9d8e27b426a6e17c245fdc172c372c2d
IT24100637 <it24100637@my.sliit.lk>
2026-04-29 13:15:45 +0530

    Change App.jsx

--- WMT-Frontend/src/App.jsx
+++ WMT-Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "./components/NavBar.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import AddItemPage from "./pages/AddItemPage.jsx";
 import EditItemPage from "./pages/EditItemPage.jsx";
